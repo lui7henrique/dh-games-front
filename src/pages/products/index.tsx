e25c778e8f3f396a1c29441bcde2a545cf1ad65e
Luiz Henrique <7henrique18@gmail.com>
2022-04-05 22:41:53 -0300

    :sparkles: Game card

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,11 +1,47 @@
-import type { NextPage } from 'next'
+import { Grid } from '@chakra-ui/react'
+import type { GetStaticProps, NextPage } from 'next'
+import { GameCard } from '../../components/GameCard'
+import { Limiter } from '../../components/Limiter'
+import { api } from '../../services/api'
+import { Game } from '../../types/game'
 
-const Products: NextPage = () => {
+type ProductsProps = {
+  games: Game[]
+}
+
+const Products = (props: ProductsProps) => {
+  const { games } = props
+
+  console.log(games)
+
   return (
     <>
-      <h1>products</h1>
+      <Limiter>
+        <Grid
+          templateColumns={{
+            base: 'repeat(1, 1fr)',
+            md: 'repeat(2, 1fr)',
+            lg: 'repeat(3, 1fr)'
+          }}
+          gap="1.5rem"
+        >
+          {games.map((game, index) => {
+            return <GameCard game={game} key={index} />
+          })}
+        </Grid>
+      </Limiter>
     </>
   )
 }
 
 export default Products
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await api.get<Game[]>('/products')
+
+  return {
+    props: {
+      games: data
+    } // will be passed to the page component as props
+  }
+}
