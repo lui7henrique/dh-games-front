6383b4333d5588b883714e93f4fd2499364e0ad0
Mario <mariomarthins@gmail.com>
2022-04-20 18:21:54 -0300

    :bug: fix it URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http://3.17.138.107:8080'
-  baseURL: 'https://dhgames.ctdprojetos.com.br/:8080'
+  baseURL: 'https://dhgames.ctdprojetos.com.br/'
 })
